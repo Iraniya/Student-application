314008a54d1487a97735a111222cfb1b17bd3a08
Iraniya <n201101047@gmail.com>
2016-10-24 13:32:39 +0530

    Please enter the commit message for your changes. Lines starting

---
+++ Student/Student/UIColor+Utility.h
@@ -0,0 +1,13 @@
+//
+//  UIColor+Utility.h
+//  Student
+//
+//  Created by agile on 06/10/16.
+//  Copyright © 2016 iraniya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Utility)
+
+@end
---
+++ Student/Student/UIColor+Utility.m
@@ -0,0 +1,13 @@
+//
+//  UIColor+Utility.m
+//  Student
+//
+//  Created by agile on 06/10/16.
+//  Copyright © 2016 iraniya. All rights reserved.
+//
+
+#import "UIColor+Utility.h"
+
+@implementation UIColor (Utility)
+
+@end
